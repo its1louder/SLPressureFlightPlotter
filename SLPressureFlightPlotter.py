cd407b0a730ca2578217f5b96b47cbc25d6738a3
its1louder <its1louder@users.noreply.github.com>
2024-10-25 11:15:16 -0700

    Update SLPressureFlightPlotter.py

--- SLPressureFlightPlotter.py
+++ SLPressureFlightPlotter.py
@@ -82,8 +82,9 @@ def main(url=URL):
         except: 
             # print error capture to streamlit, not print
             errmsg = 'failed to load data from %s. Are you sure there is data there?' %url
-            st.write(errmsg+msglst)
-            print(errmsg+msglst)
+            st.write(errmsg)
+            st.write(msglst)
+            #print(errmsg,msglst)
             raise (errmsg)
 
 
